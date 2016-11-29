b212b61cc7db41dc5a14880afc34e21484cfe302
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2016-11-29 22:17:23 +0200

    add to db script separating to solution and equation tables

--- src/main/resources/db/postgres/basic_script.sql
+++ src/main/resources/db/postgres/basic_script.sql
@@ -6,20 +6,26 @@ CREATE SEQUENCE global_sequence START 1000000;
 DROP TABLE IF EXISTS equation CASCADE;
 CREATE TABLE equation
 (
-  id      INTEGER DEFAULT nextval('global_sequence'),
+  id          INTEGER DEFAULT nextval('global_sequence'),
+  solution_id INTEGER,
 
   param_a FLOAT DEFAULT 0,
   param_b FLOAT DEFAULT 0,
   param_c FLOAT DEFAULT 0,
 
-  x_1     FLOAT DEFAULT NULL,
-  x_2     FLOAT DEFAULT NULL,
-
   has_natural_solution BOOLEAN DEFAULT NULL,
+
+  PRIMARY KEY (id),
+  FOREIGN KEY (solution_id) REFERENCES solution(id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS solution CASCADE;
+CREATE TABLE solution
+(
+  id INTEGER DEFAULT nextval('global_sequence'),
+
+  x_1 FLOAT DEFAULT NULL,
+  x_2 FLOAT DEFAULT NULL,
 
   PRIMARY KEY (id)
 );
-
-INSERT INTO equation (param_a, param_b, param_c) VALUES (31.2, 44.61, -14.76);
-
-SELECT param_a, param_b, param_c FROM equation;
