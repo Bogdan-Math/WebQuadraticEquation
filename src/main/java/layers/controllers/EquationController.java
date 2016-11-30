52e6bc347756b6904f33df91f6aa619af93c5044
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2016-11-30 22:38:13 +0200

    add equation controller

--- src/main/java/layers/controllers/EquationController.java
+++ src/main/java/layers/controllers/EquationController.java
@@ -1,16 +1,20 @@
 package layers.controllers;
 
+import model.Solution;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping(value = "/", method = RequestMethod.GET)
+@RequestMapping(value = "/")
 public class EquationController {
 
-    @RequestMapping()
-    public String root(){
-        return "index";
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Solution calculate() {
+        return new Solution(1,2);
     }
 
 }
