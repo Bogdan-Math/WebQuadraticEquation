56e63de160f0ea433779b527d360c578373d6c3d
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2016-12-01 01:18:23 +0200

    rename controller

---
+++ src/main/java/layers/controllers/EquationJsonController.java
@@ -0,0 +1,21 @@
+package layers.controllers;
+
+import model.Equation;
+import model.Solution;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(value = "/json")
+public class EquationJsonController {
+
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Solution calc(@RequestBody Equation equation) {
+
+        System.out.println(equation);
+        return new Solution(0.001, 1.4);
+    }
+
+}
