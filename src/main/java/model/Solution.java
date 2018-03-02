ddd720e3b18e5da905e0af6077d1f47c75e85524
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2018-03-02 21:14:37 +0200

    move lombok annotations to global entity scope

--- src/main/java/model/Solution.java
+++ src/main/java/model/Solution.java
@@ -8,18 +8,13 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Getter
+@Setter
 @NoArgsConstructor
 @Entity
 @Table(name = "solution")
 public class Solution extends BaseEntity {
 
-/*
-    @ManyToMany(mappedBy = "solutions")
-    private Set<Equation> equations;
-*/
-
-    @Getter
-    @Setter
     @Column(name = "x")
     private Double x;
 
