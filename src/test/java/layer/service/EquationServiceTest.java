012fc6be32b5d2b3f5190801bd72f16bfc3f9946
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2018-03-08 22:51:14 +0200

    add scratch for TDD code style

---
+++ src/test/java/layer/service/EquationServiceTest.java
@@ -0,0 +1,18 @@
+package layer.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EquationServiceTest {
+
+    @Test
+    public void solve() {
+        //TODO: add test by TDD style
+    }
+
+    @Test
+    public void save() {
+        //TODO: add test by TDD style
+    }
+}
